9c496a99d7022ff54c22547f744f68b3376c03fb
Farooq Karimi Zadeh <fkz@riseup.net>
2021-07-27 18:51:41 +0430

    converting to functional component

--- src/ui/Header.js
+++ src/ui/Header.js
@@ -1,25 +1,16 @@
 import "KaiUI/src/components/Header/Header.scss";
 import colors from "KaiUI/src/theme/colors.scss";
-import { createTextVNode, Component } from "inferno";
+import { createTextVNode } from "inferno";
 
-class Header extends Component {
-  constructor(props) {
-    super(props);
-    console.log(`[ui/Header] text: ${props.text}`);
-  }
-
-  render() {
-    return (
-      <header
-        className="kai-header"
-        style={{ background: this.props.backgroundColor || colors.headerBlue }}
-      >
-        <h1 className={"h1"} $HasVNodeChildren>
-          {createTextVNode(this.props.text)}
+function Header(props) {
+  return (
+    <header
+      className="kai-header"
+      style={{ background: props.backgroundColor || colors.headerBlue }}>
+        <h1 className="h1" $HasVNodeChildren>
+          {createTextVNode(props.text)}
         </h1>
-      </header>
-    );
-  }
+    </header>);
 }
 
 export default Header;
