e5ad6ec1279eb1f6e6dbf26552f26b0cee60186d
Farooq Karimi Zadeh <fkz@riseup.net>
2021-09-27 16:31:55 +0330

    implementing invites

--- src/InvitesView.js
+++ src/InvitesView.js
@@ -2,9 +2,18 @@ import { Component } from "inferno";
 import ListView from "ListView";
 
 class InvitesView extends Component {
+  getInvite = (room) => room.getMyMembership() === "invite";
   constructor(props) {
     super(props);
-    this.
-    this.state
+    this.invites = window.mClient.getRooms().filter(this.getInvite);
+    this.state = {
+      cursor: 0,
+    };
+  }
+
+  render() {
+    // TODO
   }
 }
+
+export default InvitesView;
