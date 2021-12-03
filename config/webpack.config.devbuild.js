4708e337c7db564e50d32557fe0fa1429aa41105
Farooq Karimi Zadeh <fkz@riseup.net>
2021-12-03 20:06:39 +0330

    disable source map generation in production and development builds

--- config/webpack.config.devbuild.js
+++ config/webpack.config.devbuild.js
@@ -73,7 +73,6 @@ module.exports = {
   mode: 'development',
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebook/create-react-app/issues/343
-  devtool: 'cheap-module-source-map',
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   entry: [
