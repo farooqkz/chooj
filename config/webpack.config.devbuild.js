f4020dac581239f7c930b5ee99550b1776127f72
Farooq Karimi Zadeh <fkz@riseup.net>
2021-12-03 20:40:53 +0330

    fix CSP

--- config/webpack.config.devbuild.js
+++ config/webpack.config.devbuild.js
@@ -73,6 +73,7 @@ module.exports = {
   mode: 'development',
   // You may want 'eval' instead if you prefer to see the compiled output in DevTools.
   // See the discussion in https://github.com/facebook/create-react-app/issues/343
+  devtool: '',
   // These are the "entry points" to our application.
   // This means they will be the "root" imports that are included in JS bundle.
   entry: [
