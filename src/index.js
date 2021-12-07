fae422c1bab1af6109d7c308f38e5a2ad1eac6e0
Farooq Karimi Zadeh <fkz@riseup.net>
2021-12-07 12:36:26 +0330

    cleanup

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import "core-js";
 import { render } from "inferno";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
 
 window.isFullScreen = false;
+// This is used to track the fullscreen state of the App
+// false means the app is not fullscreen
 window.stateStores = new Map();
 // ^ This is used to save state of components on unmount
 // and later retrieve it when the component is constructed
 
 render(<App />, document.getElementById("root"));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
