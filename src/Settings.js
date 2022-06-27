cc3d5218344c334f4932f65ba1ae70785f759612
Farooq Karimi Zadeh <fkz@riseup.net>
2022-06-27 10:12:47 +0430

    oof prettier again

--- src/Settings.js
+++ src/Settings.js
@@ -3,11 +3,14 @@ function Settings() {
     <p>
       Press center key to enable push notifification in Chooj.
       <br />
-      This is <b>unreliable</b>, <b>experimental</b> and <b>CANNOT BE UNDONE</b>.
+      This is <b>unreliable</b>, <b>experimental</b> and <b>CANNOT BE UNDONE</b>
+      .
       <br />
-      You cannot disable or enable notifications per room or sender through Chooj.
+      You cannot disable or enable notifications per room or sender through
+      Chooj.
       <br />
-      If enabling push notifications was causing problems for you, you may reinstall Chooj.
+      If enabling push notifications was causing problems for you, you may
+      reinstall Chooj.
     </p>
   );
 }
