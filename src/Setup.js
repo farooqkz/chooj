eb7b6344d40fae8bbeba10e7c2372a3ee5f8d513
xrh0905 <xrh0905@xrh0905.top>
2022-08-20 13:56:43 +0800

    Syn2?

--- src/Setup.js
+++ src/Setup.js
@@ -24,11 +24,11 @@ class Setup extends Component {
     client.once("sync", (state, prevState, res) => {
       client.startClient({ lazyLoadMembers: true });
     });
-    client.setDeviceDetails(client.getDeviceId(), { display_name: DeviceName });
+    client.setDeviceDetails(client.getDeviceId(), { display_name: DeviceName }).then(() => 
     client.once("sync", (state, prevState, res) => {
       localforage.setItem("setuped", true);
       self.refresh();
-    });
+    }));
   }
 
   refresh() {
