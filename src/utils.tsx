a09a4eecbd9b65b90b3b4620c53ad9178a60b439
Farooq Karimi Zadeh <fkz@riseup.net>
2023-05-26 08:04:33 +0330

    the previous commit also did convert this file from js to ts. this commit improves the typings

--- src/utils.tsx
+++ src/utils.tsx
@@ -1,9 +1,10 @@
-import { getHttpUriForMxc } from "matrix-js-sdk";
+import { getHttpUriForMxc, Room, MatrixClient, MatrixEvent } from "matrix-js-sdk";
 import { render } from "inferno";
+import { RoomsViewState } from "./types";
 
 const defaultAvatarSize = 36;
 
-function urlBase64ToUint8Array(base64String) {
+function urlBase64ToUint8Array(base64String: string) : Uint8Array {
   const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
   const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
 
@@ -16,10 +17,10 @@ function urlBase64ToUint8Array(base64String) {
   return outputArray;
 }
 
-function updateState(room, state) {
+function updateState(room: Room, state: RoomsViewState) : RoomsViewState {
   let isAlreadyOurRoom = false;
   // ^ is <room> a room we already have in this.state.rooms?
-  state.rooms = state.rooms.map((ourRoom) => {
+  state.rooms = state.rooms.map((ourRoom: Room) => {
     if (room.roomId === ourRoom.roomId) {
       isAlreadyOurRoom = true;
     }
@@ -31,15 +32,15 @@ function updateState(room, state) {
   return state;
 }
 
-function isDM(room) {
+function isDM(room: Room) : Boolean {
   return room.getJoinedMemberCount() === 2 && room.getMyMembership() === "join" && !room.isSpaceRoom();
 }
 
-function isRoom(room) {
+function isRoom(room: Room) : Boolean {
   return room.getJoinedMemberCount() > 2 && room.getMyMembership() === "join" && !room.isSpaceRoom();
 }
 
-function getAvatarOrDefault(mxcUrl, defaultUrl, size) {
+function getAvatarOrDefault(mxcUrl: string, defaultUrl: string, size: number) : string {
   size = size || defaultAvatarSize;
   if (mxcUrl) {
     return getHttpUriForMxc(
@@ -55,11 +56,11 @@ function getAvatarOrDefault(mxcUrl, defaultUrl, size) {
   }
 }
 
-function startDM(client, userId) {
+function startDM(client: MatrixClient, userId: string) {
   // TODO
 }
 
-function eventSender(sender, myself, dm) {
+function eventSender(sender: string, myself: string, dm?: Boolean) : string {
   if (myself === sender) {
     return "You";
   } else {
@@ -71,14 +72,14 @@ function eventSender(sender, myself, dm) {
   }
 }
 
-function makeHumanReadableEvent(evt, dm) {
+function makeHumanReadableEvent(evt: MatrixEvent, dm?: Boolean) : string {
   if (!(evt instanceof Object)) {
     console.log("BOOO", evt);
   }
-  const type = evt.getType();
-  const content = evt.getContent();
-  const sender = evt.getSender();
-  const myself = evt.getSender() === window.mClient.getUserId();
+  const type: string = evt.getType();
+  const content: object = evt.getContent();
+  const sender: string = evt.getSender();
+  const myself: Boolean = evt.getSender() === window.mClient.getUserId();
 
   switch (type) {
     case "m.call.hangup":
@@ -105,36 +106,34 @@ function makeHumanReadableEvent(evt, dm) {
   }
 }
 
-function bytesToHigherScale(b) {
-  let units = ["B", "KiB", "MiB", "GiB"];
-  let unit = 0;
+function bytesToHigherScale(b: number) : string {
+  let units: Array<string> = ["B", "KiB", "MiB", "GiB"];
+  let unit: number = 0;
   while (b >= 512 && unit < 3) {
     b /= 1024;
     unit++;
   }
-  b = b.toFixed(2);
-  return `${b}${units[unit]}`;
+  return `${b.toFixed(2)}${units[unit]}`;
 }
 
-function msToHigherScale(ms) {
-  let units = ["s", "m", "h"];
-  let unit = 0;
+function msToHigherScale(ms: number) : string {
+  let units: Array<string> = ["s", "m", "h"];
+  let unit: number = 0;
   ms /= 1000;
   while (ms >= 60) {
     ms /= 60;
     unit++;
   }
-  ms = ms.toFixed(2);
-  return `${ms}${units[unit]}`;
+  return `${ms.toFixed(2)}${units[unit]}`;
 }
 
-function mxcMediaToHttp(hsUrl, mxcUrl) {
+function mxcMediaToHttp(hsUrl: string, mxcUrl: string) : string {
   let [serverName, mediaId] = mxcUrl.split("/").slice(2, 4);
   return `${hsUrl}/_matrix/media/v3/download/${serverName}/${mediaId}`;
 }
 
-function toast(message, timeout) {
-  let container = document.querySelector("#toast");
+function toast(message: string, timeout: number) {
+  let container: HTMLElement = document.querySelector("#toast");
   container.style.display = "block";
   render(<p $HasTextChildren>{message}</p>, container);
   setTimeout(() => {
@@ -142,7 +141,7 @@ function toast(message, timeout) {
   }, timeout);
 }
 
-function readableTimestamp(ts, includeSeconds) {
+function readableTimestamp(ts: number, includeSeconds?: Boolean) : string {
   let date = new Date(ts);
   let h = date.getHours().toString();
   let m = date.getMinutes().toString();
@@ -163,12 +162,12 @@ function readableTimestamp(ts, includeSeconds) {
   return "[" + d + "] ";
 }
 
-function getRoomLastEvent(room) {
+function getRoomLastEvent(room: Room) : MatrixEvent | null {
   let events = room.getLiveTimeline().getEvents();
   return events[events.length - 1] || null;
 }
 
-function getSomeDisplayName(userId) {
+function getSomeDisplayName(userId: string) : string {
   let userObj = window.mClient.getUser(userId);
   if (userObj) {
     return userObj.displayName || userObj.userId.split(":")[0].replace("@", "");
