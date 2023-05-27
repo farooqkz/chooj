b0c8dd6350c4b614841b42dce787de88163a5a67
Farooq Karimi Zadeh <fkz@riseup.net>
2023-05-27 12:59:42 +0330

    convert InvitesView to tsx

---
+++ src/InvitesView.tsx
@@ -0,0 +1,44 @@
+import { Component } from "inferno";
+import { Room } from "matrix-js-sdk";
+import {
+  ListView,
+  TextListItem,
+} from "KaiUI";
+import NoItem from "./NoItem";
+
+
+interface InvitesViewState {
+  cursor: number;
+}
+
+interface InvitesViewProps {
+  selectedInviteCb: (invite: Room) => void;
+}
+
+class InvitesView extends Component<InvitesViewProps, InvitesViewState> {
+  getInvite = (room: Room) => room.getMyMembership() === "invite";
+  constructor(props: InvitesViewProps) {
+    super(props);
+    this.state = {
+      cursor: 0,
+    };
+  }
+
+  cursorChangeCb = (cursor: number) => {
+    //this.props.selectedInviteCb(this.invites[cursor]);
+    this.setState({ cursor: cursor });
+  };
+
+  render() {
+    return <NoItem text="Invites not implemented yet" />;
+    /*
+    return (
+      <ListView cursorChangeCb={this.cursorChangeCb} cursor={this.state.cursor}>
+        <TextListItem primary="Hello :)" focused />
+      </ListView>
+    );
+    */
+  }
+}
+
+export default InvitesView;
