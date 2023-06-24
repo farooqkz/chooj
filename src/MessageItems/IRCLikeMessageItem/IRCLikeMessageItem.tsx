f7289178ee93278cf16e6706da625a22b80c7632
Farooq Karimi Zadeh <fkz@riseup.net>
2023-06-24 17:12:45 +0330

    conv IRCLikeMessageItems

--- src/MessageItems/IRCLikeMessageItem/IRCLikeMessageItem.tsx
+++ src/MessageItems/IRCLikeMessageItem/IRCLikeMessageItem.tsx
@@ -1,16 +1,54 @@
 import { createTextVNode } from "inferno";
 import classNames from "classnames";
+import { IContent } from "matrix-js-sdk";
 import "./IRCLikeMessageItem.css";
 import {
   mxcMediaToHttp,
   bytesToHigherScale,
   msToHigherScale,
-  readableTimestamp,
   getSomeDisplayName,
 } from "../../utils";
 
+interface Sender {
+  userId: string;
+}
 
-function getClassNameFromStatus(status) {
+interface IRCLikeMessageItemCommonProps {
+  readableTs: string;
+  isFocused: Boolean;
+  status: string | null;
+}
+
+interface IRCLkeMessageItemProps {
+  content: IContent;
+  sender: Sender;
+}
+
+interface IRCLikeMessageItemTextProps {
+  text: string; 
+  sender: string;
+}
+
+interface IRCLikeMessageItemImageProps {
+  width: number;
+  height: number;
+  url: string;
+  sender: string;
+  size: number;
+}
+
+interface IRCLikeMessageItemAudioProps {
+  duration: number;
+  url: string;
+  size: number;
+  sender: string;
+}
+
+interface IRCLikeMessageItemUnknownProps {
+  sender: string;
+}
+
+function getClassNameFromStatus(status: null | string) : string {
   if (status === null) return "";
   if (status === "not_sent") {
     return "not_sent";
@@ -21,11 +59,11 @@ function getClassNameFromStatus(status) {
   }
 }
 
-function IRCLikeMessageItemText({ date, text, sender, isFocused, status }) {
+function IRCLikeMessageItemText({ readableTs, text, sender, isFocused, status }: IRCLikeMessageItemCommonProps & IRCLikeMessageItemTextProps) {
   return (
     <div className={classNames("ircmsg" + (isFocused ? "--focused" : ""), getClassNameFromStatus(status))} tabIndex={0}>
       <p>
-        <b $HasTextChildren>{date}</b>
+        <b $HasTextChildren>{readableTs}</b>
         <b $HasTextChildren>{`<${sender}>`}</b>
         {createTextVNode(" " + text)}
       </p>
@@ -33,12 +71,12 @@ function IRCLikeMessageItemText({ date, text, sender, isFocused, status }) {
   );
 }
 
-function IRCLikeMessageItemNotice({ date, isFocused, text, sender, status }) {
+function IRCLikeMessageItemNotice({ readableTs, isFocused, text, sender, status }: IRCLikeMessageItemCommonProps & IRCLikeMessageItemTextProps) {
   return (
     <div className={classNames("ircmsg" + (isFocused ? "--focused" : ""), getClassNameFromStatus(status))} tabIndex={0}>
       <p>
         <i>
-          <b $HasTextChildren>{date}</b>
+          <b $HasTextChildren>{readableTs}</b>
           <b $HasTextChildren>{`<${sender}>`}</b>
           {createTextVNode(" " + text)}
         </i>
@@ -48,7 +86,7 @@ function IRCLikeMessageItemNotice({ date, isFocused, text, sender, status }) {
 }
 
 function IRCLikeMessageItemImage({
-  date,
+  readableTs,
   sender,
   text,
   width,
@@ -56,7 +94,7 @@ function IRCLikeMessageItemImage({
   url,
   isFocused,
   status,
-}) {
+}: IRCLikeMessageItemTextProps & IRCLikeMessageItemImageProps & IRCLikeMessageItemCommonProps) {
   while (height > (192 * 2) / 3) {
     height *= 0.75;
     width *= 0.75;
@@ -69,7 +107,7 @@ function IRCLikeMessageItemImage({
   return (
     <div className={classNames("ircmsg" + (isFocused ? "--focused" : ""), getClassNameFromStatus(status))} tabIndex={0}>
       <p>
-        <b $HasTextChildren>{date}</b>
+        <b $HasTextChildren>{readableTs}</b>
         <b $HasTextChildren>{`<${sender}>`}</b>
         {createTextVNode(text)}
         <img width={width} height={height} src={url} alt={text} />
@@ -79,7 +117,7 @@ function IRCLikeMessageItemImage({
 }
 
 function IRCLikeMessageItemAudio({
-  date,
+  readableTs,
   isFocused,
   sender,
   size,
@@ -87,13 +125,13 @@ function IRCLikeMessageItemAudio({
   url,
   text,
   status,
-}) {
+}: IRCLikeMessageItemCommonProps & IRCLikeMessageItemAudioProps & IRCLikeMessageItemTextProps) {
   const hsUrl = window.mClient.getHomeserverUrl();
   url = mxcMediaToHttp(hsUrl, url);
   return (
     <div className={classNames("ircmsg" + (isFocused ? "--focused" : ""), getClassNameFromStatus(status))} tabIndex={0}>
       <p>
-        <b $HasTextChildren>{date}</b>
+        <b $HasTextChildren>{readableTs}</b>
         <b $HasTextChildren>{`${sender} has sent an audio clip.`}</b>
         <br />
         Title: {createTextVNode(text)}
@@ -101,31 +139,31 @@ function IRCLikeMessageItemAudio({
         Duration: {createTextVNode(msToHigherScale(duration))}
         <br />
         Size: {createTextVNode(bytesToHigherScale(size))}
-        <audio src={url} autoplay={false} />
+        <audio src={url} autoPlay={false} />
       </p>
     </div>
   );
 }
 
-function IRCLikeMessageItemUnknown({ date, isFocused, sender, status }) {
+function IRCLikeMessageItemUnknown({ readableTs, isFocused, sender, status }: IRCLikeMessageItemCommonProps & IRCLikeMessageItemUnknownProps) {
   return (
     <div className={classNames("ircmsg" + (isFocused ? "--focused" : ""), getClassNameFromStatus(status))} tabIndex={0}>
-      <b $HasTextChildren>{date}</b>
+      <b $HasTextChildren>{readableTs}</b>
       <p $HasTextChildren>Unsupported message type was sent from {sender}</p>
     </div>
   );
 }
 
-function IRCLikeMessageItem({ date, sender, content, isFocused, status }) {
+function IRCLikeMessageItem({ readableTs, sender, content, isFocused, status }: IRCLikeMessageItemCommonProps & IRCLkeMessageItemProps) {
   const userId = sender.userId;
   let displayName = getSomeDisplayName(userId);
   // In matrix-js-sdk 15.1.1 sometimes getUser(...) returns null. This is a temporary workaround.
-  let d = readableTimestamp(date);
+
   switch (content.msgtype) {
     case "m.text":
       return (
         <IRCLikeMessageItemText
-          date={d}
+          readableTs={readableTs}
           sender={displayName}
           text={content.body}
           status={status}
@@ -135,7 +173,7 @@ function IRCLikeMessageItem({ date, sender, content, isFocused, status }) {
     case "m.notice":
       return (
         <IRCLikeMessageItemNotice
-          date={d}
+          readableTs={readableTs}
           sender={displayName}
           text={content.body}
           status={status}
@@ -145,7 +183,7 @@ function IRCLikeMessageItem({ date, sender, content, isFocused, status }) {
     case "m.image":
       return (
         <IRCLikeMessageItemImage
-          date={d}
+          readableTs={readableTs}
           sender={displayName}
           text={content.body}
           width={content.info.w}
@@ -159,7 +197,7 @@ function IRCLikeMessageItem({ date, sender, content, isFocused, status }) {
     case "m.audio":
       return (
         <IRCLikeMessageItemAudio
-          date={d}
+          readableTs={readableTs}
           sender={displayName}
           text={content.body}
           duration={content.info.duration}
@@ -176,7 +214,7 @@ function IRCLikeMessageItem({ date, sender, content, isFocused, status }) {
     default:
       return (
         <IRCLikeMessageItemUnknown
-          date={d}
+          readableTs={readableTs}
           sender={sender.userId}
           status={status}
           isFocused={isFocused}
