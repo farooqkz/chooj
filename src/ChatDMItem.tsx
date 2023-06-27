238ba0e8f811ab77c06739786d62345b310d6928
Farooq Karimi Zadeh <fkz@riseup.net>
2023-06-27 12:22:48 +0330

    fixed ChatDMItem

--- src/ChatDMItem.tsx
+++ src/ChatDMItem.tsx
@@ -2,12 +2,13 @@ import { Component } from "inferno";
 import { Avatar, IconListItem } from "KaiUI";
 import { Room, RoomEvent, User, UserEvent, MatrixEvent } from "matrix-js-sdk";
 import personIcon from "./person_icon.png";
-import { makeHumanReadableEvent, getRoomLastEvent, getAvatarOrDefault } from "./utils";
+import { makeHumanReadableEvent, getAvatarOrDefault } from "./utils";
+import { shared } from "./shared";
 
 
 interface ChatDMItemState {
   displayName: string;
-  avatarUrl: string;
+  avatarUrl?: string;
   presence: string;
   lastEvent: string;
   lastActiveAgo: number;
@@ -39,26 +40,42 @@ export default class ChatDMItem extends Component<ChatDMItemProps, ChatDMItemSta
     });
   };
 
-  updateTimeline = (evt: MatrixEvent, _room: Room) => { 
+  updateTimeline = (_evt: MatrixEvent, room: Room | undefined) => { 
+    if (!room) return;
+    let lastEvent: MatrixEvent | undefined = room.getLastLiveEvent();
+    if (!lastEvent) return;
     this.setState({
-      lastEvent: makeHumanReadableEvent(evt, true)
+      lastEvent: makeHumanReadableEvent(lastEvent, true)
     });
   }
 
   constructor(props: ChatDMItemProps) {
     super(props);
-    let user = window.mClient.getUser(props.room.guessDMUserId());
+    if (!shared.mClient) {
+      throw new Error("mClient is null");
+    }
+    let user: User | null = shared.mClient.getUser(props.room.guessDMUserId());
+    if (!user) {
+      throw Error("User not found");
+    }
+    let lastEvent: MatrixEvent | undefined = props.room.getLastLiveEvent();
     this.state = {
       presence: user.presence,
       lastActiveAgo: user.lastActiveAgo,
       avatarUrl: user.avatarUrl,
-      displayName: user.displayName,
-      lastEvent: makeHumanReadableEvent(getRoomLastEvent(props.room), true),
+      displayName: user.displayName || user.userId,
+      lastEvent: (lastEvent && makeHumanReadableEvent(lastEvent, true)) || "",
     };
   }
 
   componentDidMount() {
-    let user: User = window.mClient.getUser(this.props.room.guessDMUserId());
+    if (!shared.mClient) {
+      throw new Error("mClient is null");
+    }
+    let user: User | null = shared.mClient.getUser(this.props.room.guessDMUserId());
+    if (!user) {
+      return;
+    }
     user.on(UserEvent.Presence, this.updatePresence);
     user.on(UserEvent.AvatarUrl, this.updateAvatar);
     user.on(UserEvent.DisplayName, this.updateDisplayName);
@@ -66,7 +83,10 @@ export default class ChatDMItem extends Component<ChatDMItemProps, ChatDMItemSta
   }
 
   componentWillUnmount() {
-    let user: User = window.mClient.getUser(this.props.room.guessDMUserId());
+    let user: User | null = shared.mClient.getUser(this.props.room.guessDMUserId());
+    if (!user) {
+      return;
+    }
     user.off(UserEvent.Presence, this.updatePresence);
     user.off(UserEvent.AvatarUrl, this.updateAvatar);
     user.off(UserEvent.DisplayName, this.updateDisplayName);
