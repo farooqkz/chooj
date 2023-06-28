91df779bf6d9c6339b720f4cc906bd80f0690d26
Farooq Karimi Zadeh <fkz@riseup.net>
2023-06-28 14:59:40 +0330

    trying to define some Kai specific stuff

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,3 +1,27 @@
 declare module "*.png";
 declare module "*.ogg";
 declare module "xmimetype";
+
+declare global {
+  interface Navigator {
+    volumeManager: VolumeManager; 
+    getDeviceStorage: (deviceStorage: ValidDeviceStorages) => DeviceStorage;
+  }
+  interface Window {
+    MozActivity: MozActivity; 
+  }
+  interface VolumeManager {
+    requestShow: () => void;
+  }
+
+  interface MozActivity {
+    new (options: MozActivityOptions): MozActivity;
+    onerror?: (error: any) => void;
+  }
+
+  interface MozActivityOptions {
+    name: string;
+    data?: any;
+  }
+  type ValidDeviceStorages = "apps" | "music" | "pictures" | "videos" | "sdcard";
+}
