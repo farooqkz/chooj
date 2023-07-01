8fcaf3943287300cfc94130c7f95616740a0bf3d
Farooq Karimi Zadeh <fkz@riseup.net>
2023-07-01 21:36:42 +0330

    StepZero done

---
+++ src/Guide/StepZero.tsx
@@ -0,0 +1,11 @@
+function StepZero() {
+  return (
+    <div className="step">
+      <p>
+        Hello! Care to spend 30 seconds to learn about chooj and its underlying network, Matrix?
+      </p>
+    </div>
+  );
+}
+
+export default StepZero;
