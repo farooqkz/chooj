959701b2b3734ea6e93331504d1f6f8bc6b7b68d
Farooq Karimi Zadeh <fkz@riseup.net>
2023-07-02 11:43:48 +0330

    StepFour done

---
+++ src/Guide/StepFour.tsx
@@ -0,0 +1,11 @@
+function StepFour() {
+  return (
+    <div className="step">
+      <p>
+        You can use chooj with an existing account on some homeserver to chat in different rooms on the Matrix network. If you haven't got an account, don't worry! You can use guest access through a homeserver to join rooms which allow guest access.
+      </p>
+    </div>
+  );
+}
+
+export default StepFour;
