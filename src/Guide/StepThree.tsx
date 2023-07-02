716e9a5298f28ea43aab42491bd4349892114445
Farooq Karimi Zadeh <fkz@riseup.net>
2023-07-02 11:43:31 +0330

    StepThree done

---
+++ src/Guide/StepThree.tsx
@@ -0,0 +1,11 @@
+function StepThree() {
+  return (
+    <div className="step">
+      <p>
+        You can join any of these "homeservers" or host your own homeserver for yourself, and possibly family and friends. To use chooj, to connect to the Matrix network, you should connect to a Matrix homeserver.
+      </p>
+    </div>
+  );
+}
+
+export default StepThree;
