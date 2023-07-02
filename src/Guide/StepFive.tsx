1bc735ea42e6c13804e582d85bbfb9ea7893e069
Farooq Karimi Zadeh <fkz@riseup.net>
2023-07-02 11:44:09 +0330

    StepFive done

---
+++ src/Guide/StepFive.tsx
@@ -0,0 +1,11 @@
+function StepFive() {
+  return (
+    <div className="step">
+      <p>
+        You can also register an account on some homeserver of your own choice. However, chooj currently does not support guest access or account registration. But it's coming soon!
+      </p>
+    </div>
+  );
+}
+
+export default StepFive;
