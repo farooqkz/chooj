2775ceba006c3a0ab350bf1ead07e1280bd8b211
Farooq Karimi Zadeh <fkz@riseup.net>
2023-08-04 13:58:52 +0330

    add a TODO comment

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { render } from "inferno";
-import "core-js";
+import "core-js"; // TODO: import only what is needed not everything
 import "abortcontroller-polyfill/dist/polyfill-patch-fetch";
 import "./index.css";
 import App from "./App";
