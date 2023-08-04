af516a73358fef2e4edee4f34bc1637bfa1aa79e
Farooq Karimi Zadeh <fkz@riseup.net>
2023-08-04 13:47:59 +0330

    improve NoItem

--- src/NoItem.tsx
+++ src/NoItem.tsx
@@ -1,13 +1,13 @@
+import "./NoItem";
+
 interface NoItemProps {
   text: string;
 }
 
 export default function NoItem({ text }: NoItemProps) {
   return (
-    <div style={{ "margin-top": "20vh" }}>
-      <p style={{ "text-align": "center" }}>
-        <b>{text}</b>
-      </p>
+    <div className="noitem">
+      <h1 $HasTextChildren>{text}</h1>
     </div>
   );
 }
